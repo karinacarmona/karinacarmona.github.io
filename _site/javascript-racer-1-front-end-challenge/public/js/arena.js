208bb87384e953c32e93f08e8671dde1240266d9
Karina Carmona <karinacarmona@me.com>
2015-01-20 16:59:41 -0500

    remove racer

--- _site/javascript-racer-1-front-end-challenge/public/js/arena.js
+++
@@ -1,8 +0,0 @@
-function Arena(height, width) {
-  this.height = height;
-  this.width = width;
-  this.$element = $('<div id="arena"></div>');
-  this.$element.css('height', height);
-  this.$element.css('width', width);
-  $('body').append(this.$element);
-}
